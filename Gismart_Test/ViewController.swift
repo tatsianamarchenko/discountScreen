c0dddec90da79b5f5bd5fcfa71320fef20e79f82
Tatsiana <tanya3308225@mail.ru>
2022-06-29 20:15:18 +0300

    Initial Commit

---
+++ Gismart_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gismart_Test
+//
+//  Created by Tatsiana Marchanka on 29.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
