252653a176810414a9268c7ffb2f856b3c62673f
Tatsiana <tanya3308225@mail.ru>
2022-06-30 14:14:44 +0300

    added UI

---
+++ Gismart_Test/Timer.swift
@@ -0,0 +1,169 @@
+//
+//  Timer.swift
+//  Gismart_Test
+//
+//  Created by Tatsiana Marchanka on 30.06.22.
+//
+
+import Foundation
+import UIKit
+
+class TimerView: UIView {
+
+	var timer: Timer?
+	private var timerValue = 86400
+
+	var dayLable: UILabel!
+	var hoursLable: UILabel!
+	var minutesLable: UILabel!
+	var secondsLable: UILabel!
+
+	private	func createView() -> UIView {
+		let view = UIView()
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.backgroundColor = #colorLiteral(red: 0.1686274707, green: 0.1686274707, blue: 0.1686274707, alpha: 1)
+		view.layer.cornerRadius = 12
+		return view
+	}
+
+	private func createColonLable () -> UILabel {
+		let lable = UILabel()
+		lable.text = ":"
+		lable.textColor = .white
+		lable.textAlignment = .center
+		lable.translatesAutoresizingMaskIntoConstraints = false
+		return lable
+	}
+
+	private	func createLable() -> UILabel {
+		let lable = UILabel()
+		lable.textColor = .white
+		lable.textAlignment = .center
+		lable.translatesAutoresizingMaskIntoConstraints = false
+		return lable
+	}
+
+	func runTimer() {
+		timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+	}
+
+	@objc func updateTimer() {
+		if timerValue < 1 {
+			timer?.invalidate()
+		} else {
+			timerValue -= 1
+			timeString(time: TimeInterval(timerValue), handler: { [weak self] (days, hours, minutes, seconds) in
+				self?.dayLable.text = days
+				self?.hoursLable.text = hours
+				self?.minutesLable.text = minutes
+				self?.secondsLable.text = seconds
+			})
+		}
+	}
+
+	private func timeString(time:TimeInterval, handler: @escaping (String, String, String, String) -> Void) {
+		let days = Int(time) / 86400
+		let hours = Int(time) / 3600
+		let minutes = Int(time) / 60 % 60
+		let seconds = Int(time) % 60
+		handler(String(format:"%02i", days),
+				String(format:"%02i", hours),
+				String(format:"%02i", minutes),
+				String(format:"%02i", seconds))
+	}
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+
+		dayLable = createLable()
+		hoursLable = createLable()
+		minutesLable = createLable()
+		secondsLable = createLable()
+		runTimer()
+
+		let dayView = createView()
+		let hoursView = createView()
+		let minutesView = createView()
+		let secondsView = createView()
+		let firstColon = createColonLable()
+		let secondColon = createColonLable()
+		let thirdColon = createColonLable()
+		dayView.addSubview(dayLable)
+		hoursView.addSubview(hoursLable)
+		minutesView.addSubview(minutesLable)
+		secondsView.addSubview(secondsLable)
+		addSubview(dayView)
+		addSubview(firstColon)
+		addSubview(hoursView)
+		addSubview(secondColon)
+		addSubview(minutesView)
+		addSubview(thirdColon)
+		addSubview(secondsView)
+
+		NSLayoutConstraint.activate([
+			dayView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 3),
+			dayView.heightAnchor.constraint(equalToConstant: 41),
+			dayView.widthAnchor.constraint(equalToConstant: 62.5)
+		])
+
+		NSLayoutConstraint.activate([
+			dayLable.centerXAnchor.constraint(equalTo: dayView.centerXAnchor),
+			dayLable.centerYAnchor.constraint(equalTo: dayView.centerYAnchor)
+		])
+
+		NSLayoutConstraint.activate([
+			hoursLable.centerXAnchor.constraint(equalTo: hoursView.centerXAnchor),
+			hoursLable.centerYAnchor.constraint(equalTo: hoursView.centerYAnchor)
+		])
+
+		NSLayoutConstraint.activate([
+			minutesLable.centerXAnchor.constraint(equalTo: minutesView.centerXAnchor),
+			minutesLable.centerYAnchor.constraint(equalTo: minutesView.centerYAnchor)
+		])
+
+		NSLayoutConstraint.activate([
+			secondsLable.centerXAnchor.constraint(equalTo: secondsView.centerXAnchor),
+			secondsLable.centerYAnchor.constraint(equalTo: secondsView.centerYAnchor)
+		])
+
+		NSLayoutConstraint.activate([
+			firstColon.leadingAnchor.constraint(equalTo: dayView.trailingAnchor, constant: 3),
+			firstColon.heightAnchor.constraint(equalToConstant: 41),
+			firstColon.widthAnchor.constraint(equalToConstant: 3)
+		])
+
+		NSLayoutConstraint.activate([
+			hoursView.leadingAnchor.constraint(equalTo: firstColon.trailingAnchor, constant: 3),
+			hoursView.heightAnchor.constraint(equalToConstant: 41),
+			hoursView.widthAnchor.constraint(equalToConstant: 62.5)
+		])
+
+		NSLayoutConstraint.activate([
+			secondColon.leadingAnchor.constraint(equalTo: hoursView.trailingAnchor, constant: 3),
+			secondColon.heightAnchor.constraint(equalToConstant: 41),
+			secondColon.widthAnchor.constraint(equalToConstant: 3)
+		])
+
+		NSLayoutConstraint.activate([
+			minutesView.leadingAnchor.constraint(equalTo: secondColon.trailingAnchor, constant: 3),
+			minutesView.heightAnchor.constraint(equalToConstant: 41),
+			minutesView.widthAnchor.constraint(equalToConstant: 62.5)
+		])
+
+		NSLayoutConstraint.activate([
+			thirdColon.leadingAnchor.constraint(equalTo: minutesView.trailingAnchor, constant: 3),
+			thirdColon.heightAnchor.constraint(equalToConstant: 41),
+			thirdColon.widthAnchor.constraint(equalToConstant: 3)
+		])
+
+		NSLayoutConstraint.activate([
+			secondsView.leadingAnchor.constraint(equalTo: thirdColon.trailingAnchor, constant: 3),
+			secondsView.heightAnchor.constraint(equalToConstant: 41),
+			secondsView.widthAnchor.constraint(equalToConstant: 62.5)
+		])
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
--- Gismart_Test/ViewController.swift
+++ Gismart_Test/ViewController.swift
@@ -9,11 +9,190 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+	lazy private var musicImage: UIImageView = {
+		let image =	UIImage(named: "music")!
+		let imageView = UIImageView(image: image)
+		imageView.contentMode = .scaleAspectFit
+		imageView.translatesAutoresizingMaskIntoConstraints = false
+		return imageView
+	}()
+
+	lazy private var saleTitleLable: UILabel = {
+		var lable = UILabel()
+		lable.text = "LAST-MINUTE CHANCE! to claim your offer"
+		lable.textColor = .white
+		lable.font = .systemFont(ofSize: 22, weight: .semibold)
+		lable.translatesAutoresizingMaskIntoConstraints = false
+		lable.textAlignment = .center
+		lable.backgroundColor = .systemMint
+		lable.lineBreakStrategy = .standard
+		lable.lineBreakMode = .byTruncatingHead
+		lable.numberOfLines = 2
+		return lable
+	}()
+
+	lazy private var salePercentLable: UILabel = {
+		var lable = UILabel()
+		lable.text = "90% OFF"
+		lable.textColor = .white
+		lable.font = .systemFont(ofSize: 55, weight: .black)
+		lable.translatesAutoresizingMaskIntoConstraints = false
+		lable.textAlignment = .center
+		lable.numberOfLines = 1
+		return lable
+	}()
+
+	lazy private var forLable: UILabel = {
+		var lable = UILabel()
+		lable.text = "For true music fans"
+		lable.textColor = .white
+		lable.font = .systemFont(ofSize: 15, weight: .semibold)
+		lable.translatesAutoresizingMaskIntoConstraints = false
+		lable.textAlignment = .center
+		lable.numberOfLines = 1
+		return lable
+	}()
+
+	lazy private var numberOfSongsLable: UILabel = {
+		var lable = UILabel()
+		lable.text = "Hundreds of songs in your pocket"
+		lable.textColor = .white
+		lable.font = .systemFont(ofSize: 14, weight: .regular)
+		lable.translatesAutoresizingMaskIntoConstraints = false
+		lable.textAlignment = .center
+		lable.numberOfLines = 1
+		return lable
+	}()
+
+	lazy private var activateButton: UIButton = {
+		var button = ActualGradientButton()
+		button.setTitle("ACTIVATE OFFER", for: .normal)
+		button.titleLabel?.font = .systemFont(ofSize: 15, weight: .semibold)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		button.layer.cornerRadius = 12
+
+		button.layer.shadowColor = UIColor.systemPink.cgColor
+		button.layer.shadowRadius = 30
+		button.layer.shadowOpacity = 0.8
+		button.layer.masksToBounds = false
+
+		return button
+	}()
+
+	lazy private var privacyButton = createButtomButtons(text: "Privacy")
+	lazy private var restoreButton = createButtomButtons(text: "Restore")
+	lazy private var termsButton = createButtomButtons(text: "Terms")
+
+	private func createButtomButtons(text: String) -> UIButton {
+		let button = UIButton()
+		button.setTitle(text, for: .normal)
+		button.setTitleColor(.gray, for: .normal)
+		button.titleLabel?.font = .systemFont(ofSize: 10, weight: .regular)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		return button
 	}
 
+	//let clock = TimerView()
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.addSubview(musicImage)
+		view.addSubview(saleTitleLable)
+		view.addSubview(salePercentLable)
+		view.addSubview(forLable)
+		view.addSubview(numberOfSongsLable)
+		view.addSubview(activateButton)
+		view.addSubview(privacyButton)
+		view.addSubview(restoreButton)
+		view.addSubview(termsButton)
+		timer.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(timer)
+		makeConstraints()
+		view.backgroundColor = .black
+	}
+	var timer = TimerView()
+
+	func makeConstraints() {
+		NSLayoutConstraint.activate([
+			musicImage.topAnchor.constraint(equalTo: view.topAnchor),
+			musicImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+			musicImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			musicImage.trailingAnchor.constraint(equalTo: view.centerXAnchor)
+		])
+
+		NSLayoutConstraint.activate([
+			saleTitleLable.topAnchor.constraint(equalTo: view.topAnchor),
+			saleTitleLable.leadingAnchor.constraint(equalTo: activateButton.leadingAnchor, constant: 30),
+		//	saleTitleLable.widthAnchor.constraint(equalToConstant: 260),
+			saleTitleLable.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30)
+		])
+
+		NSLayoutConstraint.activate([
+			salePercentLable.topAnchor.constraint(equalTo: saleTitleLable.bottomAnchor, constant: 12),
+			salePercentLable.leadingAnchor.constraint(equalTo: saleTitleLable.leadingAnchor),
+			salePercentLable.trailingAnchor.constraint(equalTo: saleTitleLable.trailingAnchor)
+		])
+
+		NSLayoutConstraint.activate([
+			forLable.topAnchor.constraint(equalTo: salePercentLable.bottomAnchor, constant: 8),
+			forLable.leadingAnchor.constraint(equalTo: saleTitleLable.leadingAnchor),
+			forLable.trailingAnchor.constraint(equalTo: saleTitleLable.trailingAnchor)
+		])
+
+		NSLayoutConstraint.activate([
+			timer.topAnchor.constraint(equalTo: forLable.bottomAnchor, constant: 16),
+			timer.leadingAnchor.constraint(equalTo: activateButton.leadingAnchor, constant: 10)
+		])
+
+		NSLayoutConstraint.activate([
+			numberOfSongsLable.topAnchor.constraint(equalTo: timer.bottomAnchor, constant: 56),
+			numberOfSongsLable.leadingAnchor.constraint(equalTo: activateButton.leadingAnchor),
+			numberOfSongsLable.trailingAnchor.constraint(equalTo: activateButton.trailingAnchor)
+		])
+
+		NSLayoutConstraint.activate([
+			activateButton.topAnchor.constraint(equalTo: numberOfSongsLable.bottomAnchor, constant: 10),
+			activateButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+			activateButton.heightAnchor.constraint(equalToConstant: 63),
+			activateButton.widthAnchor.constraint(equalToConstant: 300)
+		])
+
+		NSLayoutConstraint.activate([
+			privacyButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+			privacyButton.trailingAnchor.constraint(equalTo: restoreButton.leadingAnchor, constant: -10),
+		])
+
+		NSLayoutConstraint.activate([
+			restoreButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+			restoreButton.centerXAnchor.constraint(equalTo: activateButton.centerXAnchor),
+		])
+
+		NSLayoutConstraint.activate([
+			termsButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+			termsButton.leadingAnchor.constraint(equalTo: restoreButton.trailingAnchor, constant: 10),
+		])
+	}
+	
 }
 
+class ActualGradientButton: UIButton {
+
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		gradientLayer.frame = bounds
+	}
+
+	private lazy var gradientLayer: CAGradientLayer = {
+		let l = CAGradientLayer()
+		l.frame = self.bounds
+		let blue = #colorLiteral(red: 0.2575358152, green: 0.2706360817, blue: 0.6008356214, alpha: 1).cgColor
+		let pink = #colorLiteral(red: 0.987001121, green: 0.2012993395, blue: 0.7515467405, alpha: 1).cgColor
+		l.colors = [blue, pink]
+		l.type = .axial
+		l.startPoint = CGPoint(x: 0, y: 0.5)
+		l.endPoint = CGPoint(x: 1, y: 0.5)
+		l.cornerRadius = 16
+		layer.insertSublayer(l, at: 0)
+		return l
+	}()
+}
